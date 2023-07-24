5ec6fc89c52fe4780611929b5b471f2bce0f3e5a
Dejan1990 <dejan.simo@hotmail.com>
2023-07-24 21:41:23 +0200

    Check winner function added

--- main.py
+++ main.py
@@ -117,7 +117,34 @@ class Game:
             print("*" * 30)
             player_hand.display()
             dealer_hand.display()
-    
+            
+            
+    def check_winner(self, player_hand, dealer_hand, game_over=False):
+        if not game_over:
+            if player_hand.get_value() > 21:
+                print("You busted. Dealer wins! 😭")
+                return True
+            elif dealer_hand.get_value() > 21:
+                print("Dealer busted. You win! 😀")
+                return True
+            elif dealer_hand.is_blackjack() and player_hand.is_blackjack():
+                print("Both players have blackjack! Tie! 😑")
+                return True
+            elif player_hand.is_blackjack():
+                print("You have blackjack. You win! 😀")
+                return True
+            elif dealer_hand.is_blackjack():
+                print("Dealer has blackjack. Dealer wins! 😭")
+                return True
+        else:
+            if player_hand.get_value() > dealer_hand.get_value():
+                print("You win! 😀")
+            elif player_hand.get_value() == dealer_hand.get_value():
+                print("Tie! 😑")
+            else:
+                print("Dealer wins. 😭")
+            return True
+        return False
     
 g = Game()
 g.play()
