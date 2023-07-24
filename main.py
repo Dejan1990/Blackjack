5032cc20fd6a5c3879381905d47fc77203993667
Dejan1990 <dejan.simo@hotmail.com>
2023-07-24 21:52:19 +0200

    Finish blackjack game

--- main.py
+++ main.py
@@ -118,6 +118,43 @@ class Game:
             player_hand.display()
             dealer_hand.display()
             
+            if self.check_winner(player_hand, dealer_hand):
+                continue # go to next iteration (while loop)
+            
+            choice = ""
+            while player_hand.get_value() < 21 and choice not in ["s", "stand"]:
+                choice = input("Please choose 'Hit' or 'Stand': ").lower()
+                print()
+                while choice not in ["h", "s", "hit", "stand"]:
+                    choice = input("Please enter 'Hit' or 'Stand' (or H/S) ").lower()
+                    print()
+                if choice in ["hit", "h"]:
+                    player_hand.add_card(deck.deal(1))
+                    player_hand.display()
+                    
+            if self.check_winner(player_hand, dealer_hand):
+                continue
+            
+            player_hand_value = player_hand.get_value()
+            dealer_hand_value = dealer_hand.get_value()
+
+            while dealer_hand_value < 17:
+                dealer_hand.add_card(deck.deal(1))
+                dealer_hand_value = dealer_hand.get_value()
+
+            dealer_hand.display(show_all_dealer_cards=True)
+
+            if self.check_winner(player_hand, dealer_hand):
+                continue
+
+            print("Final Results")
+            print("Your hand:", player_hand_value)
+            print("Dealer's hand:", dealer_hand_value)
+
+            self.check_winner(player_hand, dealer_hand, True)
+
+        print("\nThanks for playing!")
+            
             
     def check_winner(self, player_hand, dealer_hand, game_over=False):
         if not game_over:
